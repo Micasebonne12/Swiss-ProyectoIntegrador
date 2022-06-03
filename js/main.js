386857a6389bab8436048451d93fc82940bbfb5f
Micaela Casebonne <micaela.casebonne@arbusta.net>
2022-06-03 18:05:08 -0300

    Aplico JQuery para cambiar vistas segun px de pantalla, para abrir y cerrar listas, y cambio img por svg en index para futuras modificaciones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
-$('.chevron').click(function () {
-    $('.lista').hide();
-    $('.chevron').click(function(){
-      $('.lista').show();
-    });
+// Función que detecta el tamaño de pantalla para mostrar u ocultar vistas
+$( window ).resize(()=> {
+  let width = $( window ).width();
+  if ( width < 735){
+    $('main').show();
+    $('section').show();
+  }else{
+    $('section').hide();
+  }
+ });
+
+ // Función para que al hacer click en el ícono de flechas, cambie la vista de imagen por especificaciones
+$('.icon-arrow').click(()=>{
+  $('main').toggle()
+  $('section').toggle()
+}) 
+
+// Funciones para que según el título en el que se haga click, se cambie por una u otra vista
+$('.title-tech').click(function(){
+  $('section').toggle();
+  $('main').toggle();
+})
+
+$('.subtitle-box').click(function(){
+  $('main').toggle();
+  $('section').toggle();
+})
+
+// Funciones para que se abran y cierren las listas en versión mobile
+$('.chevron1').click(function () {
+    $('.lista1').toggle();
   }); 
+$('.chevron2').click(function () {
+    $('.lista2').toggle();
+  }); 
+$('.chevron3').click(function () {
+    $('.lista3').toggle();
+  }); 
+$('.chevron4').click(function () {
+    $('.lista4').toggle();
+  });
